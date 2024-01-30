741c31b501dad561adee6befcf916c03b9865025
Andrey Spiney <96716371+Father1993@users.noreply.github.com>
2024-01-30 12:49:54 +1000

    fix img bugs

--- main.js
+++ main.js
@@ -66,42 +66,43 @@ function startGame(count) {
         }
         return pair;
     }
-    
+
     function shuffle(array) {
         let shuffledArray = [...array];
         for (let i = shuffledArray.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
-            [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+            [shuffledArray[i], shuffledArray[j]] = [
+                shuffledArray[j],
+                shuffledArray[i],
+            ];
         }
         return shuffledArray;
     }
-    
+
     function createCardElements(pairArray) {
         const cardContainer = document.getElementById("card-container");
-    
+
         pairArray.forEach((number) => {
             const card = document.createElement("div");
             const cardFront = document.createElement("div");
             const cardBack = document.createElement("div");
-    
+
             card.classList.add("card");
-            card.classList.add("col-md-6")
+            card.classList.add("col-md-6");
             cardFront.classList.add("card-front");
             cardBack.classList.add("card-back");
-    
-    
+
             cardBack.textContent = number;
-    
+
             cardContainer.appendChild(card);
             card.appendChild(cardFront);
             card.appendChild(cardBack);
-    
-            card.addEventListener('click', () => {
-    
-                if (!card.classList.contains('check') && openCard.length < 2) {
-                    card.classList.toggle('check');
+
+            card.addEventListener("click", () => {
+                if (!card.classList.contains("check") && openCard.length < 2) {
+                    card.classList.toggle("check");
                     openCard.push(card);
-        
+
                     if (openCard.length === 2) {
                         checkForMatch();
                     }
@@ -110,18 +111,18 @@ function startGame(count) {
         });
     }
 
-    function checkForMatch() {     
+    function checkForMatch() {
         const card_one = openCard[0];
         const card_two = openCard[1];
 
-        const number_one = card_one.querySelector('.card-back').textContent;
-        const number_two = card_two.querySelector('.card-back').textContent;
+        const number_one = card_one.querySelector(".card-back").textContent;
+        const number_two = card_two.querySelector(".card-back").textContent;
 
         if (number_one === number_two) {
             mathPairs.push(number_one);
             if (mathPairs.length === count) {
                 // The End
-                alert('You WIN!');
+                alert("You WIN!");
                 clearUI();
                 stopTimer();
                 gameStarted = false;
@@ -129,8 +130,8 @@ function startGame(count) {
         } else {
             // timeout
             setTimeout(() => {
-                card_one.classList.remove('check');
-                card_two.classList.remove('check');
+                card_one.classList.remove("check");
+                card_two.classList.remove("check");
             }, 1000);
         }
         openCard = [];
@@ -159,7 +160,7 @@ level_2.addEventListener("click", () => {
 
 level_3.addEventListener("click", () => {
     const cardContainer = document.getElementById("game__windows");
-    cardContainer.style.width = "1030px"; 
+    cardContainer.style.width = "1030px";
     resetGame();
     startGame(14);
 });
